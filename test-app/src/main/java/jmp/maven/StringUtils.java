1c3cbe297ed02942abcdce65216dd9f3a64efbc5
alsh00007 <apache817@mail.ru>
2016-03-15 12:41:33 +0300

    sonar issue

--- test-app/src/main/java/jmp/maven/StringUtils.java
+++ test-app/src/main/java/jmp/maven/StringUtils.java
@@ -4,8 +4,6 @@ package jmp.maven;
  * Utility class for working with strings.
  */
 public class StringUtils {
-public static final String test ="fail";
-    public static final String test2 ="fail";
     /**
      * Method for concatenation 2 strings.
      * 
